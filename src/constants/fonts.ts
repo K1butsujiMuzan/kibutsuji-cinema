d0e65adf62c0c100c84f15d9dd39bea5f729084f
Maksim Shishkin <maks_shishkin_2007@inbox.ru>
2025-12-24 16:26:11 +0200

    fix[fonts]: fix fonts paths for vercel

--- src/constants/fonts.ts
+++ src/constants/fonts.ts
@@ -4,22 +4,22 @@ export const fontAtyp = localFont({
   variable: '--font-atyp',
   src: [
     {
-      path: '../../public/fonts/AtypRegular.woff2',
+      path: '../fonts/AtypRegular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/AtypMedium.woff2',
+      path: '../fonts/AtypMedium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/AtypSemiBold.woff2',
+      path: '../fonts/AtypSemiBold.woff2',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/AtypBold.woff2',
+      path: '../fonts/AtypBold.woff2',
       weight: '700',
       style: 'normal',
     },
