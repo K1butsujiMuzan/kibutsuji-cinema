5285eb5cfbeb5f87a9d744618212a39e8144f16c
Maksim Shishkin <maks_shishkin_2007@inbox.ru>
2026-01-31 00:50:13 +0200

    fix[cors]: add delete method to cors

--- src/lib/cors.ts
+++ src/lib/cors.ts
@@ -2,7 +2,7 @@ import type { NextResponse } from 'next/server'
 
 export function cors(response: NextResponse) {
   response.headers.set('Access-Control-Allow-Origin', '*')
-  response.headers.set('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+  response.headers.set('Access-Control-Allow-Methods', 'GET,POST,OPTIONS, DELETE')
   response.headers.set(
     'Access-Control-Allow-Headers',
     'Content-Type, Authorization',
