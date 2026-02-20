e99ddc17dd82ab91294502f56f12c9d6e6dce5f7
Maksim Shishkin <maks_shishkin_2007@inbox.ru>
2026-02-20 16:30:05 +0200

    fix[episodes]: fix episodes update endpoint

--- src/app/api/episodes/route.ts
+++ src/app/api/episodes/route.ts
@@ -224,7 +224,7 @@ export async function PUT(request: NextRequest) {
       return trimCheck.error
     }
 
-    const [trimmedAnimeId, trimmedTitle] = trimCheck.data
+    const [trimmedTitle, trimmedAnimeId] = trimCheck.data
 
     const episodeById = await prisma.animeEpisode.findUnique({ where: { id } })
 
